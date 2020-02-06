0c7b128ce66d17f2336b7321f67fdaceaa36b90c
Dano <dano@dcolombo.com>
2020-02-05 21:52:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 4000;
 const app = express();
 
 mongoose.connect(
-    "mongodb+srv://USERNAME:PASSWORD@rogueintel-ox4iq.mongodb.net/test",
+    "mongodb+srv://udemy:Admin@rogueintel-ox4iq.mongodb.net/test",
     { useNewUrlParser: true, useUnifiedTopology: true }
 );
 mongoose.connection.once("open", () => {
